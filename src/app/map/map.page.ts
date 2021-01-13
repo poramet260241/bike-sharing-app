358b18ba11c06dd034fdefa6cea2f1bcb7693762
poramet260241 <oblivion2408@gmail.com>
2021-01-13 16:38:51 +0700

    bike-sharing-app can run, solve a marker problem

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -4,10 +4,6 @@ import {Router} from '@angular/router';
 import {BikeService} from '../shared/bike.service';
 import {Bike} from '../shared/bike';
 import {ViewChild, ElementRef} from '@angular/core';
-import {AlertController, Platform} from '@ionic/angular';
-import {Plugins} from '@capacitor/core';
-
-const { App } = Plugins;
 
 declare var google: any;
 
@@ -20,6 +16,7 @@ declare var google: any;
 export class MapPage implements OnInit, AfterViewInit{
 
     bikes: Bike[] = [];
+    bikeTemp: Bike;
     firstRead = false;
     map: any;
     @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
@@ -31,52 +28,104 @@ export class MapPage implements OnInit, AfterViewInit{
         private bikeService: BikeService,
         private router: Router,
         public authenticationService: AuthenticationService,
-        private platform: Platform,
-        private alertController: AlertController,
-    ) {
-        this.platform.backButton.subscribeWithPriority(-1, () => {
-            this.presentAlert();
-        });
-    }
+    ) { }
 
     private readAllBike(){
+        this.markersArray = [];
         this.bikeService.getAllBike().snapshotChanges().subscribe(res => {
             this.bikes.length = 0;
             res.forEach(t => {
                 const bike = t.payload.toJSON();
-                bike[`$key`] = t.key;
-                bike[`isChange`] = true;
+                // bike[`$key`] = t.key;
+                // bike[`isChange`] = true;
                 this.bikes.push(bike as Bike);
             });
-            // console.log(this.bikes);
+
             console.log(this.bikes);
             this.addMarkersToMap(this.bikes);
         });
     }
 
-    async presentAlert() {
-        const alert = await this.alertController.create({
-            cssClass: 'my-custom-class',
-            header: 'Sign Out',
-            message: 'Do you want to sign out?',
-            buttons: [
-                {
-                    text: 'Cancel',
-                    role: 'cancel',
-                    cssClass: 'secondary',
-                    handler: () => {
-                        console.log('Confirm Cancel: blah');
-                    }
-                }, {
-                    text: 'Yes',
-                    handler: () => {
-                        console.log('Confirm Okay');
-                        App.exitApp();
-                    }
+    findObject(mkArr, value): any{
+        for (let i = 0; i < mkArr.length; i++){
+            if (mkArr[i].title === value ){
+                return mkArr[i];
+            }
+        }
+        return null;
+    }
+    addMarkersToMap(bikes){
+        bikes.forEach(bike => {
+            // const found = this.markersArray.find( {title} === bike.dev_id);
+            const found: any = this.findObject(this.markersArray, bike.dev_id);
+            const location = new google.maps.LatLng(bike.latitude, bike.longitude);
+            let mkIcon;
+            let mkVisible: boolean;
+            if (this.authenticationService.getCurrentUser()){
+               mkVisible = true;
+            }
+            else {
+                if (bike.speed >= 0.5){
+                    mkVisible = false;
                 }
-            ]
+                else{
+                    mkVisible = true;
+                }
+            }
+            console.log('bike in put is ' + bike);
+
+            if (bike.speed >= 0.5) {
+                mkIcon = {
+                    url: '../assets/app_marker/bike-marker-orange.png',
+                    scaledSize: new google.maps.Size(50, 50),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(25, 50)
+                };
+            }
+            else {
+                mkIcon = {
+                    url: '../assets/app_marker/bike-marker-sky.png',
+                    scaledSize: new google.maps.Size(50, 50),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(25, 50)
+                };
+            }
+            console.log(found);
+            if (found === null){
+                console.log('found is null');
+                const marker = new google.maps.Marker({
+                    position: location,
+                    title: bike.dev_id,
+                    speed: bike.speed,
+                    icon: mkIcon,
+                    visible: mkVisible,
+                });
+                marker.setVisible(mkVisible);
+                marker.setMap(this.map);
+                this.addInfoWindowToMarker(marker);
+                this.markersArray.push(marker);
+                console.log('markerArray = ' + this.markersArray);
+            }
+            else {
+                console.log('found data in object');
+                found.setPosition(location);
+                found.setIcon(mkIcon);
+                found.setVisible(mkVisible);
+            }
         });
-        await alert.present();
+    }
+
+    addInfoWindowToMarker(marker) {
+        const infoWindow = new google.maps.InfoWindow({
+            content: marker.title,
+        });
+
+        marker.addListener('click', () => {
+            this.closeAllInfoWindow();
+            infoWindow.open(this.map, marker);
+        });
+
+        this.infoWindows.push(infoWindow);
     }
 
     ngOnInit() {
@@ -93,7 +142,6 @@ export class MapPage implements OnInit, AfterViewInit{
         this.showMap();
     }
 
-
     showMap(){
         const location = new google.maps.LatLng(7.007924490508865, 100.50050999922345);
         const options = {
@@ -105,73 +153,12 @@ export class MapPage implements OnInit, AfterViewInit{
         console.log('Show map');
     }
 
-    addMarkersToMap(markers){
-        for ( let i = 0; i < this.markersArray.length; i++){
-            this.markersArray[i].setMap(null);
-        }
-        this.markersArray = [];
-        console.log('length = ' + this.markersArray.length);
-        for (const marker of markers){
-            // console.log(marker);
-            const location = new google.maps.LatLng(marker.latitude, marker.longitude);
-            let mkIcon;
-            if (marker.speed >= 0.5) {
-                mkIcon = {
-                    url: '../assets/app_marker/bike-marker-orange.png',
-                    scaledSize: new google.maps.Size(50, 50),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(25, 50)
-                };
-            }
-            else {
-                mkIcon = {
-                    url: '../assets/app_marker/bike-marker-sky.png',
-                    scaledSize: new google.maps.Size(50, 50),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(25, 50)
-                };
-            }
-            const mapMarker = new google.maps.Marker({
-                position: location,
-                title: marker.dev_id,
-                speed: marker.speed,
-                icon: mkIcon,
-            });
-            this.markersArray.push(mapMarker);
-        }
-        for ( let i = 0; i < this.markersArray.length; i++){
-            if (this.markersArray[i].speed >= 0.5){
-                if (this.authenticationService.getCurrentUser()){
-                    this.markersArray[i].setMap(this.map);
-                    this.addInfoWindowToMarker(this.markersArray[i]);
-                }
-            }
-            else{
-                this.markersArray[i].setMap(this.map);
-                this.addInfoWindowToMarker(this.markersArray[i]);
-            }
-        }
-    }
-
     floatBtnClick() {
         if (this.authenticationService.getCurrentUser()) {
             this.router.navigate(['user-detail']);
         } else {
             this.router.navigate(['app-login']);
         }
-    }
-
-    addInfoWindowToMarker(marker) {
-        const infoWindow = new google.maps.InfoWindow({
-            content: marker.title,
-        });
-
-        marker.addListener('click', () => {
-            this.closeAllInfoWindow();
-            infoWindow.open(this.map, marker);
-        });
-
-        this.infoWindows.push(infoWindow);
     }
 
     closeAllInfoWindow() {
